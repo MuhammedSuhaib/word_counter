b99227779ab81b43467fe14341e1fe99b705f2c5
Suhaib Umair <154515857+MuhammedSuhaib@users.noreply.github.com>
2024-05-08 23:58:22 +0500

    coolest word counter

---
+++ main.ts
@@ -0,0 +1,19 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const  answer: {
+    sentence: string;
+} = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Write/paste your sentence: "
+    }
+]);
+  
+const words = answer.sentence.trim().split(" ");
+
+console.log("\n", chalk.greenBright(words) , "\n");
+
+
+console.log(chalk.yellow.italic.bold(` number of words in your sentence is : \n \n \t \t "${chalk.magenta(words.length)}"`));
